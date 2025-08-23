6b4814c7ba0eca974e1641a6391dacd65e2a67f0
Patrick Doane <patrickdoane@gmail.com>
2025-08-23 07:06:07 -0500

    Add Item Model

--- grail-server/src/main/java/com/d2/grail_server/model/Item.java
+++ grail-server/src/main/java/com/d2/grail_server/model/Item.java
@@ -1,5 +1,77 @@
 package com.d2.grail_server.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
 public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String name;
+
+	private String type;
+	private String quality;
+	private String rarity;
+	private String description;
+	private String d2Version;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getQuality() {
+		return quality;
+	}
+
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+
+	public String getRarity() {
+		return rarity;
+	}
+
+	public void setRarity(String rarity) {
+		this.rarity = rarity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getD2Version() {
+		return d2Version;
+	}
+
+	public void setD2Version(String d2Version) {
+		this.d2Version = d2Version;
+	}
 
 }

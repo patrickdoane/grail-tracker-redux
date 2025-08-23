672c3b2fadd6827a48e165754de66b4ea0d0f6bc
Patrick Doane <patrickdoane@gmail.com>
2025-08-23 07:29:06 -0500

    Add ItemProperty model

--- grail-server/src/main/java/com/d2/grail_server/model/ItemProperty.java
+++ grail-server/src/main/java/com/d2/grail_server/model/ItemProperty.java
@@ -1,5 +1,52 @@
 package com.d2.grail_server.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item_properties")
 public class ItemProperty {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "item_id", nullable = false)
+	private Item item;
+
+	private String propertyName;
+	private String propertyValue;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public String getPropertyName() {
+		return propertyName;
+	}
+
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+
+	public String getPropertyValue() {
+		return propertyValue;
+	}
+
+	public void setPropertyValue(String propertyValue) {
+		this.propertyValue = propertyValue;
+	}
 
 }

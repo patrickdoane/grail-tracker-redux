15a3192ac3a49adaea947f357a579f411e1eae49
Patrick Doane <patrickdoane@gmail.com>
2025-08-23 07:20:07 -0500

    Add ItemSource model

--- grail-server/src/main/java/com/d2/grail_server/model/ItemSource.java
+++ grail-server/src/main/java/com/d2/grail_server/model/ItemSource.java
@@ -1,5 +1,53 @@
 package com.d2.grail_server.model;
 
+import jakarta.persistence.*;
+
+// Hibernate maps this model to the item_sources table
+
+@Entity
+@Table(name = "item_sources")
 public class ItemSource {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "item_id", nullable = false)
+	private Item item;
+
+	private String sourceType;
+	private String sourceName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public String getSourceType() {
+		return sourceType;
+	}
+
+	public void setSourceType(String sourceType) {
+		this.sourceType = sourceType;
+	}
+
+	public String getSourceName() {
+		return sourceName;
+	}
+
+	public void setSourceName(String sourceName) {
+		this.sourceName = sourceName;
+	}
 
 }

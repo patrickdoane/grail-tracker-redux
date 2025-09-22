9ca8f9bfcf39c5a774c9bdc2509a191c74ef179d
Patrick Doane <patrickdoane@gmail.com>
2025-09-22 09:52:00 -0500

    Add Tanstack React Query Devtools

--- grail-client/src/main.tsx
+++ grail-client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import App from './App.tsx'
 import { queryClient } from './lib/queryClient.ts'
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 )
